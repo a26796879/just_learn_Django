b69e0db9e719ef75a290d3f443862f181f58d311
a26796879 <33983387+a26796879@users.noreply.github.com>
2022-05-02 17:43:14 +0800

    add filter view for news model

--- news/views.py
+++ news/views.py
@@ -3,5 +3,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from news.serializers import NewsSerializer
 from .models import news_crawler
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
-
+class News_Filter(generics.ListAPIView):
+    queryset = news_crawler.objects.all()
+    serializer_class = NewsSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'publisher']
