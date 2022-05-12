86dc2a32261439915f68aa00d22cc0f6bc92b4ff
a26796879 <33983387+a26796879@users.noreply.github.com>
2022-05-12 21:40:54 +0800

    reset celery task settings

--- just_learn/celery.py
+++ just_learn/celery.py
@@ -6,8 +6,9 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "just_learn.settings")
 
-app = Celery("just_learn")
-
+#app = Celery("just_learn")
+app = Celery('tasks', broker='amqp://niceguy:niceguy@35.227.175.4:5672')
+app.conf.update(worker_pool_restarts=True)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
