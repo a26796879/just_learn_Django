6ed38658aaeedccb98756246e84eeb998ae57ce8
a26796879 <33983387+a26796879@users.noreply.github.com>
2022-05-12 21:41:09 +0800

    add CELERY_BEAT_SCHEDULER

--- just_learn/settings.py
+++ just_learn/settings.py
@@ -133,3 +133,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
